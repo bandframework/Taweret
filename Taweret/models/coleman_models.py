e699face5b07461bc4a1828263e27f06af565498
GitHub Action <action@github.com>
2022-12-06 13:38:37 -0500

    changing the coleman model to be compatible with our new base classes

--- Taweret/models/coleman_models.py
+++ Taweret/models/coleman_models.py
@@ -1,9 +1,15 @@
 import numpy as np
+from Taweret.core.base_model import BaseModel
+from Taweret.utils.utils import normal_log_likelihood_elementwise as log_likelihood_elementwise_utils
+import bilby
 
 
-class coleman_model_1():
+class coleman_model_1(BaseModel):
 
-    def predict(self, input_values : np.array, model_param : np.array) -> np.array:
+    def __init__(self) -> None:
+        self._prior = None
+
+    def evaluate(self, input_values : np.array, model_param : np.array) -> np.array:
         """
         Predict the mean and error for given input values
 
@@ -25,9 +31,40 @@ class coleman_model_1():
         mean = 0.5 * (x + model_param.item()) - 2
         return mean, np.sqrt(var)
 
-class coleman_model_2():
+    def log_likelihood_elementwise(self, x_exp, y_exp, y_err, model_param):
+        return log_likelihood_elementwise_utils(self, x_exp, y_exp, y_err, model_param)
+    
+    def set_prior(self, bilby_priors=None):
+        '''
+        Set the prior on model parameters.
+        '''
+        if bilby_priors is None:
+            print('Using default priors for model 1')
+            priors = bilby.prior.PriorDict()
+            priors['model1_0']=bilby.core.prior.Uniform(1, 6, "model1_0")
+        else:
+            priors = bilby_priors
+        print(priors)
+        self._prior=priors
+        return priors
 
-    def predict(self, input_values : np.array, model_param : np.array) -> np.array:
+    @property
+    def prior(self):
+        if self._prior is None:
+            return self.set_prior()
+        else:
+            return self._prior
+
+    @prior.setter
+    def prior(self, bilby_prior_dic):
+        return self.set_prior(bilby_prior_dic)
+
+class coleman_model_2(BaseModel):
+
+    def __init__(self) -> None:
+        self._prior = None
+        
+    def evaluate(self, input_values : np.array, model_param : np.array) -> np.array:
         """
         Predict the mean and error for given input values
 
@@ -47,11 +84,39 @@ class coleman_model_2():
             raise TypeError('The model_param has to be single element numpy array')
 
         mean = -0.5 * (x - model_param.item()) + 3.7
-        return mean, np.sqrt(var)      
+        return mean, np.sqrt(var)  
 
-class coleman_truth():
+    def log_likelihood_elementwise(self, x_exp, y_exp, y_err, model_param):
+        return log_likelihood_elementwise_utils(self, x_exp, y_exp, y_err, model_param)
+    
+    def set_prior(self, bilby_priors=None):
+        '''
+        Set the prior on model parameters.
+        '''
+        if bilby_priors is None:
+            print('Using default priors for model 2')
+            priors = bilby.prior.PriorDict()
+            priors['model2_0']=bilby.core.prior.Uniform(-2, 3, "model2_0")
+        else:
+            priors = bilby_priors
+        print(priors)
+        self._prior=priors
+        return priors
 
-    def predict(self, input_values : np.array) -> np.array:
+    @property
+    def prior(self):
+        if self._prior is None:
+            return self.set_prior()
+        else:
+            return self._prior
+
+    @prior.setter
+    def prior(self, bilby_prior_dic):
+        return self.set_prior(bilby_prior_dic)
+
+class coleman_truth(BaseModel):
+
+    def evaluate(self, input_values : np.array) -> np.array:
         """
         Predict the mean and error for given input values
 
@@ -67,3 +132,9 @@ class coleman_truth():
 
         mean = 2 - 0.1 * (x - 4)**2
         return mean, np.sqrt(var)
+
+    def log_likelihood_elementwise(self, x_exp, y_exp, y_err, model_param):
+        pass
+
+    def set_prior(self, bilby_priors=None):
+        pass

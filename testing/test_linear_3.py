c549275aa353566a4cfea10f3a45265595d1b3d4
ominusliticus <elkevin111@gmail.com>
2023-02-04 17:43:02 -0500

    Local Linear Mixing: Adding test file (eventaully moved to Jupyter)

---
+++ testing/test_linear_3.py
@@ -0,0 +1,12 @@
+import _mypackage
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from Taweret.core.base_model import BaseModel
+from Taweret.mix.linear import LinearMixerLocal
+
+import my_plotting as mp
+
+# Basic example form Coleman thesis for parameter inference
+# Then in a separate file, an example using the SAMBA models

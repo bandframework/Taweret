86b3139207b96c0b171d4d146b4c5b7cf555fc4d
Alexandra Semposki <as727414@ohio.edu>
2023-05-08 15:50:49 -0400

    changing for the tutorial

--- Taweret/models/samba_models.py
+++ Taweret/models/samba_models.py
@@ -7,10 +7,10 @@ import sys
 from Taweret.core.base_model import BaseModel
 from Taweret.utils.utils import normal_log_likelihood_elementwise as log_likelihood_elementwise_utils
 
-#sys.path.append("../SAMBA")
+sys.path.append("../SAMBA")
 
-#from samba import models   # assuming you have SAMBA in your Taweret top directory 
-#from samba import mixing
+# from samba import models   # assuming you have SAMBA in your Taweret top directory 
+# from samba import mixing
 
 try:
     from samba import models   # assuming you have SAMBA in your Taweret top directory 
@@ -151,7 +151,7 @@ class highorder(BaseModel):
         Not needed for this model. 
         '''
         return None
-    
+
 
 class true_model(BaseModel):
     """

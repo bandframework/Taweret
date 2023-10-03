c9ce3d6f791cdb899704865817da79ed6a9aa5c7
Alexandra Semposki <as727414@ohio.edu>
2023-10-02 21:13:15 -0400

    adding correlated models

--- Taweret/mix/gaussian.py
+++ Taweret/mix/gaussian.py
@@ -13,7 +13,9 @@ class Multivariate(BaseMixer):
     r'''
     The multivariate BMM class originally introduced
     in the BAND SAMBA package. Combines individual
-    models using a Gaussian form.
+    models using a Gaussian form. Now able to handle
+    correlated models (up to the 3 model case). 
+    ### Extend to N models later ###
 
     .. math::
 
@@ -45,7 +47,7 @@ class Multivariate(BaseMixer):
 
         n_models : int
             Number of free parameters per model.
-
+            
         Returns:
         --------
         None.
@@ -114,7 +116,7 @@ class Multivariate(BaseMixer):
         '''
         return None
 
-    def predict(self, ci=68):
+    def predict(self, ci=68, correlated=False, R=0, S=0, T=0):
         '''
         The f_dagger function responsible for mixing the models together
         in a Gaussian way. Based on the first two moments of the
@@ -124,6 +126,19 @@ class Multivariate(BaseMixer):
         -----------
         ci : int, list
             The desired credibility interval(s) (1-sigma, 2-sigma)
+            
+        correlated : bool
+            If the models are correlated, will choose the correlated
+            model mixing formula. Default is False.
+        
+        R : int, float
+            The correlation parameter between model 1 and model 2.
+        
+        S : int, float
+            The correlation parameter between model 2 and model 3.
+            
+        T : int float
+            The correlation parameter between model 1 and model 3.
 
         Returns:
         --------
@@ -131,6 +146,9 @@ class Multivariate(BaseMixer):
             The mean, credible intervals, and std_dev
             of the predicted mixed model
         '''
+        
+        # set var_weights to zero
+        self.var_weights = np.zeros([len(self.models), len(self.x)])
 
         # credibility interval(s)
         self.ci = ci
@@ -148,25 +166,94 @@ class Multivariate(BaseMixer):
         for i in range(len(self.models)):
             f.append(self.prediction[i][0].flatten())
             v.append(np.square(self.prediction[i][1]).flatten())
+            
+        # decide whether we use correlated or not
+        if correlated is True:
+            
+            # for 2 models
+            if self.n_models == 2:
+                R = R
+                
+                # separate the arrays again
+                f_1 = np.asarray(f[0])
+                f_2 = np.asarray(f[1])
+                                
+                var_1 = np.asarray(np.sqrt(v[0]))
+                var_2 = np.asarray(np.sqrt(v[1]))
 
-        # initialise arrays
-        num = np.zeros(len(self.x))
-        denom = np.zeros(len(self.x))
-        var = np.zeros(len(self.x))
+                mean_n = np.zeros([len(self.x)])
+                mean = np.zeros([len(self.x)])
+                v_plus = np.zeros([len(self.x)])
+                var = np.zeros([len(self.x)])
+                var_n = np.zeros([len(self.x)])
 
-        # sum over the models in the same input space
-        for i in range(len(self.x)):
-            num[i] = np.sum([f[j][i] / v[j][i] for j in range(len(f))])
-            denom[i] = np.sum([1 / v[j][i] for j in range(len(f))])
+                # write up the mean and variance for the correlated model now
+                mean_n = var_1**2.0 * f_2 + var_2**2.0 * f_1 - (R * var_1 * var_2 * (f_1 + f_2))
 
-        # combine everything via input space tracking
-        mean = num / denom
-        var = 1 / denom
+                v_plus = var_1**2.0 - 2.0 * R * var_1 * var_2 + var_2**2.0
 
-        # variances for each model
-        v = np.asarray(v)
-        weights = 1.0 / v
-        self.var_weights = weights / np.sum(weights, axis=0)
+                mean = mean_n/v_plus
+                var_n = (var_1**2.0 * var_2**2.0 * (1.0 - R**2.0))
+                
+                var = var_n/v_plus
+            
+            # for 3 models
+            if self.n_models == 3:
+                R = R
+                S = S
+                T = T
+                
+                # separate the arrays again
+                f_1 = np.asarray(f[0])
+                f_2 = np.asarray(f[1])
+                f_3 = np.asarray(f[2])
+                                
+                var_1 = np.asarray(np.sqrt(v[0]))
+                var_2 = np.asarray(np.sqrt(v[1]))
+                var_3 = np.asarray(np.sqrt(v[2]))
+                
+                #initialise arrays
+                mean_n = np.zeros([len(self.x)])
+                mean = np.zeros([len(self.x)])
+                v_plus = np.zeros([len(self.x)])
+                var_n = np.zeros([len(self.x)])
+                var = np.zeros([len(self.x)])
+
+                # cofactor matrix elements
+                v_plus = (var_2**2.0 * var_3**2.0 * (1.0 - S**2.0)) + (var_1**2.0 * var_3**2.0 * (1.0 - T**2.0)) \
+                + (var_1**2.0 * var_2**2.0 * (1.0 - R**2.0)) - 2.0*(var_1*var_2*var_3**2.0 * (R - S*T))  \
+                + 2.0*(var_1*var_2**2.0*var_3 * (R*S - T)) - 2.0*(var_1**2.0*var_2*var_3 * (S - R*T))
+
+                # mean function
+                mean_n = (var_2**2.0 * var_3**2.0 * (1.0 - S**2.0) * f_1) + (var_1**2.0 * var_3**2.0 * (1.0 - T**2.0) * f_2) \
+                + (var_1**2.0 * var_2**2.0 * (1.0 - R**2.0) * f_3) - (var_1*var_2*var_3**2.0 * (R - S*T) * (f_1 + f_2))  \
+                + (var_1*var_2**2.0*var_3 * (R*S - T) * (f_1 + f_3)) - (var_1**2.0*var_2*var_3 * (S - R*T) * (f_2 + f_3))
+                mean = mean_n/v_plus
+
+                # variance function
+                var_n = (var_1**2.0*var_2**2.0*var_3**2.0 * (1.0 - R**2 - T**2 - S**2 + 2*R*S*T))
+                var = var_n/v_plus
+            
+        else:
+
+            # initialise arrays
+            num = np.zeros(len(self.x))
+            denom = np.zeros(len(self.x))
+            var = np.zeros(len(self.x))
+
+            # sum over the models in the same input space
+            for i in range(len(self.x)):
+                num[i] = np.sum([f[j][i] / v[j][i] for j in range(len(f))])
+                denom[i] = np.sum([1 / v[j][i] for j in range(len(f))])
+
+            # combine everything via input space tracking
+            mean = num / denom
+            var = 1 / denom
+
+            # variances for each model
+            v = np.asarray(v)
+            weights = 1.0 / v
+            self.var_weights = weights / np.sum(weights, axis=0)
 
         # std_dev calculation
         std_dev = np.sqrt(var)

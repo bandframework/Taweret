433534437c6b19603a0dedd625b1d8fbc8b77d3e
Jared O'Neal <joneal@anl.gov>
2024-08-27 10:11:57 -0500

    (Issue #100) Make flake8 happy.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
 import Taweret
+# import os
 # import re
 
 # -- Project information -----------------------------------------------------

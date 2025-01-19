9c5e6d44defde6154c9a09e4c50b8590a87a4d22
Jared O'Neal <joneal@anl.gov>
2025-01-19 09:55:31 -0600

    (Issue #95) Limit number of MPI processes for trees tests.
    See comment added in this commit.  I had to make this same change in the
    OpenBTMixing repo.

--- src/Taweret/tests/test_trees.py
+++ src/Taweret/tests/test_trees.py
@@ -65,6 +65,11 @@ def test_mixing():
     assert mix.obt.inform_prior == False, "class object inform_prior is not set."
 
     # Train the model
+    #
+    # The GitHub action runners can have as few as two processors.  When tests
+    # run on those with Open MPI with more MPI processes than processors, it
+    # exits due to oversubscription.  The value of tc is set to get all Open
+    # MPI-based actions running.
     fit = mix.train(
         X=x_train,
         y=y_train,
@@ -73,7 +78,7 @@ def test_mixing():
         nskip=2000,
         adaptevery=500,
         minnumbot=4,
-        tc = 4)
+        tc = 2)
 
     # Check the mcmc objects
     assert mix.obt.ndpost == 10000, "class object ndpost is not set."

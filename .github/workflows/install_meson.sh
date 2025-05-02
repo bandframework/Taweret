c457300bd8fa3bbb48df89637059b187714c7721
Jared O'Neal <joneal@anl.gov>
2025-05-02 12:55:54 -0500

    (Issue #95) Try installing meson via pip with macOS as well.

--- .github/workflows/install_meson.sh
+++ .github/workflows/install_meson.sh
@@ -10,6 +10,10 @@ install_path=$1
 runner_os=$2
 
 # Beginning with v1.6.0 meson can automatically find OpenMPI and MPICH
+venv_path=$install_path/local/venv
+meson_venv=$venv_path/meson
+local_bin=$install_path/local/bin
+
 if   [ "$runner_os" = "macOS" ]; then
     # Homebrew already has >= v1.6.0 available.
     #
@@ -17,44 +21,38 @@ if   [ "$runner_os" = "macOS" ]; then
     # installation is fixed to a Python v3.13 supposedly installed by homebrew.
     # Therefore, meson is not installed for Python 3.12 and calling meson fails
     # when trying to build a wheel.
-    brew list
-    brew uninstall python@3.13
-    brew install --build-from-source meson
-    which python
-    python -m pip list
-    cat /opt/homebrew/bin/meson
+    #
+    # Unfortunately, we install as for Ubuntu now...
+    brew update
+    brew install ninja
 elif [ "$runner_os" = "Linux" ]; then
     # Meson versions available through Ubuntu package installation can be quite
     # out-of-date.
-    venv_path=$install_path/local/venv
-    meson_venv=$venv_path/meson
-    local_bin=$install_path/local/bin
-
     sudo apt-get update
     sudo apt-get -y install ninja-build
-
-    echo " "
-    mkdir -p $venv_path
-    mkdir -p $local_bin
-    
-    python -m venv $meson_venv
-    . $meson_venv/bin/activate
-    which python
-    which pip
-    python -m pip install --upgrade pip setuptools
-    python -m pip install meson>=1.6.0
-    echo " "
-    python -m pip list
-    echo " "
-    deactivate
-    
-    # Install just meson command in path for all subsequent steps in job
-    ln -s $meson_venv/bin/meson $local_bin
-    echo "$local_bin" >> "$GITHUB_PATH"
-    echo " "
 else
     echo
     echo "Invalid runner OS $runner_os"
     echo
     exit 1
 fi
+
+echo " "
+mkdir -p $venv_path
+mkdir -p $local_bin
+
+python -m venv $meson_venv
+. $meson_venv/bin/activate
+which python
+which pip
+python -m pip install --upgrade pip setuptools
+python -m pip install meson>=1.6.0
+echo " "
+python -m pip list
+echo " "
+deactivate
+
+# Install just meson command in path for all subsequent steps in job
+ln -s $meson_venv/bin/meson $local_bin
+echo "$local_bin" >> "$GITHUB_PATH"
+echo " "
